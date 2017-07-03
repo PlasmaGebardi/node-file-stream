2c9abd68b3fabcffa173bc7389da4791bedf4cd0
Määttänen Matti <maattma@ixonos.local>
2017-07-03 14:26:38 +0300

    skeleton for the service

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var app = express();
+
+var dir = path.join(__dirname, (process.env.DIR || 'media'));
+
+app.set('port', process.env.PORT || 1337);
+app.use(express.static(dir));
+
+app.get('/', function (req, res){
+  fs.readdir(dir, (err, files) => {
+    var randomFile = files[Math.floor(Math.random() * files.length)];
+    console.log('Sending random file from dir: ' + randomFile);
+    res.sendFile(dir + '\\' + randomFile);
+  });
+});
+
+app.get('/:index(\\d+)/', function(req, res){
+  console.log('Sending file number: ' + req.params.index);
+  fs.readdir(dir, (err, files) => {
+    if (req.params.index >= files.length) { res.send('Index out of bounds'); }
+    else { res.sendFile(dir + '\\' + files[req.params.index]); }
+  });
+});
+
+var server = app.listen(app.get('port'), function(){
+  console.log('Server running on port ' + server.address().port);
+  console.log('Serving path: ' + dir);
+});
